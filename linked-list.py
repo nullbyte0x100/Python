0d68a529f22a2a593ff1a61a7368460c7d43546a
Malesi-Trevor <101787383+Malesi-Trevor@users.noreply.github.com>
2022-04-14 18:40:17 +0300

    Go

--- linked-list.py
+++ linked-list.py
@@ -1,30 +1,22 @@
+from tkinter.messagebox import NO
+
+
 class Node:
     def __init__(self,data=None):
         self.data = data
         self.next=None
 class Linked:
-    def __init_(self):
+    def  __init__(self):
         self.head=None
-    def printList(self):
+    def  printList(self):
         value=self.head
         while value is not None:
             print(value.data)
             value=value.next
-    # insert at beginning
-    def atBeginning(self,beginningData):
-        beginningNode=Node(beginningData)
-        beginningNode.next=self.head
-        self.head=beginningNode
-    # insert at the end
-    def  atEnd(self,endData):
-        endNode=Node(endData)
-        if self.head is None:
-            self.head=endNode
-            return
-        lastValue=self.head
-        while (lastValue.next):
-            lastValue.next=endNode
-        lastValue.next=endNode
+    def atBeginning(self,beginData):
+        beginNode=Node(beginData)
+        beginNode.next=self.head
+        self.head=beginNode
 l=Linked()
 l.head=Node("Monday")
 day_2=Node("Tuesday")
@@ -33,5 +25,4 @@ l.head.next=day_2
 day_2.next=day_3
 day_3.next=None
 l.atBeginning("Days of the week")
-l.atEnd("Thursday")
 l.printList()
